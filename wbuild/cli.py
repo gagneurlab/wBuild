77206dd60061f35cc81ad16f6866aa37dde78c8e
Avsecz <zigaavsec@gmail.com>
2017-12-14 15:29:17 +0100

    chore: nicer logging of the main cmd

--- wbuild/cli.py
+++ wbuild/cli.py
@@ -7,6 +7,10 @@ import wbuild
 import pathlib
 import shutil
 import distutils.dir_util
+import click_log
+import logging
+logger = logging.getLogger(__name__)
+click_log.basic_config(logger)
 
 
 def setup_paths():
@@ -19,13 +23,8 @@ def setup_paths():
 
 
 @click.group()
+@click_log.simple_verbosity_option(logger)
 def main():
-    # main command. We could also have some standard flags like --debug:
-    #
-    # @click.group()
-    # @click.option('--debug/--no-debug', default=False)
-    # def main(debug):
-    #     click.echo('Debug mode is %s' % ('on' if debug else 'off'))
     pass
 
 
@@ -39,12 +38,16 @@ def init():
     This will create a .wBuild/ folder in the current path
     """
     # TODO - check if .git exists - warn if it doesn't
+    if not os.path.exists(".git"):
+        logger.warn("Not in a .git repository")
+    if os.path.exists(".wBuild"):
+        logger.error("ERROR: .wBuild already exists")
+        # TODO exit
     templatePath, wbuildPath, demoPath = setup_paths()
     distutils.dir_util.copy_tree(str(wbuildPath), './.wBuild')
-    click.echo("init...done")
+    logger.info("init...done")
 
 
-# TODO - add the directory path as optional
 @main.command()
 def demo():
     """Setup a demo wBuild demo project
@@ -55,7 +58,7 @@ def demo():
     shutil.copy(str(templatePath / 'readme.md'), '.')
     distutils.dir_util.copy_tree(str(wbuildPath), './.wBuild')
     distutils.dir_util.copy_tree(str(demoPath), '.')
-    click.echo("demo...done")
+    logger.info("demo...done")
 
 
 @main.command()
@@ -68,8 +71,9 @@ def update():
     if not os.path.exists(".wBuild"):
         raise ValueError(".wBuild doesn't exists. Please run wBuild init first or move to the right directory")
 
-    click.echo("Removing .wBuild")
+    logger.info("Removing .wBuild")
     shutil.rmtree(".wBuild")
-    click.echo("Running .Init")
-    init()
-    click.echo("update...done")
+    logger.info("Running .Init")
+    templatePath, wbuildPath, demoPath = setup_paths()
+    distutils.dir_util.copy_tree(str(wbuildPath), './.wBuild')
+    logger.info("update...done")
