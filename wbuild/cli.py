3b1fb39b4d0ead09d53ddde2d6548db2e871f9a2
Avsecz <zigaavsec@gmail.com>
2017-12-14 15:14:08 +0100

    feat: added the update command

--- wbuild/cli.py
+++ wbuild/cli.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """CLI interface to wbuild."""
 
+import os
 import click
 import wbuild
 import pathlib
@@ -33,13 +34,21 @@ def main():
 
 @main.command()
 def init():
+    """Initialize the repository with wbuild.
+
+    This will create a .wBuild/ folder in the current path
+    """
+    # TODO - check if .git exists - warn if it doesn't
     templatePath, wbuildPath, demoPath = setup_paths()
     distutils.dir_util.copy_tree(str(wbuildPath), './.wBuild')
-    click.echo("Init...done")
+    click.echo("init...done")
 
 
+# TODO - add the directory path as optional
 @main.command()
 def demo():
+    """Setup a demo wBuild demo project
+    """
     templatePath, wbuildPath, demoPath = setup_paths()
     shutil.copy(str(templatePath / 'Snakefile'), '.')
     shutil.copy(str(templatePath / 'make.config'), '.')
@@ -47,3 +56,20 @@ def demo():
     distutils.dir_util.copy_tree(str(wbuildPath), './.wBuild')
     distutils.dir_util.copy_tree(str(demoPath), '.')
     click.echo("demo...done")
+
+
+@main.command()
+def update():
+    """Update the .wBuild folder to the most recent version of wBuild
+    """
+    # - check if .wBuild exists
+    #   - if it doesn't, return the error
+    # - if it does, update it
+    if not os.path.exists(".wBuild"):
+        raise ValueError(".wBuild doesn't exists. Please run wBuild init first or move to the right directory")
+
+    click.echo("Removing .wBuild")
+    shutil.rmtree(".wBuild")
+    click.echo("Running .Init")
+    init()
+    click.echo("update...done")
