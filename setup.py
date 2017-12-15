03c03cb848401b4e0239b6b3e0debc1171222ec2
leo <wachutka@in.tum.de>
2017-12-15 13:02:12 +0100

    Updated to version 1.0

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ test_requirements = [
 
 setup(
     name='wbuild',
-    version='0.1.4',
+    version='1.0',
     description="Automtic build tool for R Reports",
     long_description=readme + '\n\n' + history,
     author="Leonhard Wachutka",
