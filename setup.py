bf4af487d0ebc9742ee5c288a48af5be73d77a72
leo <wachutka@in.tum.de>
2018-06-25 11:42:57 +0200

    Bump version: 1.1.6 → 1.2.0

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ test_requirements = [
 
 setup(
     name='wbuild',
-    version='1.1.6',
+    version='1.2.0',
     description="Automatic build tool for R Reports",
     long_description=readme + '\n\n' + history,
     author="Leonhard Wachutka",
