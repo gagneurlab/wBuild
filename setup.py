8bcf5637adf1d3bb1d425852de6889d970b70f13
leo <wachutka@in.tum.de>
2019-09-24 15:58:56 +0200

    Bump version: 1.5.3 → 1.5.4

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ test_requirements = [
 
 setup(
     name='wbuild',
-    version='1.5.3',
+    version='1.5.4',
     description="Automatic build tool for R Reports",
     long_description=readme + '\n\n' + history,
     author="Leonhard Wachutka",
--- wbuild/__init__.py
+++ wbuild/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Leonhard Wachutka"""
 __email__ = 'leonhard@wachutka.eu'
-__version__ = '1.5.3'
+__version__ = '1.5.4'
 
 
 from . import autolink
