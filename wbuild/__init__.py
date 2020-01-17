338edf89f0f5c5dbba7217fe7d065cba2ba77a17
stefanches7 <dvoretsk@in.tum.de>
2020-01-17 14:21:29 +0100

    Bump version: 1.6.6 → 1.6.7

--- wbuild/__init__.py
+++ wbuild/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Leonhard Wachutka"""
 __email__ = 'leonhard@wachutka.eu'
-__version__ = '1.6.6'
+__version__ = '1.6.7'
 
 
 from . import autolink
--- wbuild/cli.py
+++ wbuild/cli.py
@@ -24,7 +24,7 @@ def setup_paths():
 
 @click.group()
 @click_log.simple_verbosity_option(logger)
-@click.version_option('1.6.6',prog_name='wBuild')
+@click.version_option('1.6.7',prog_name='wBuild')
 def main():
     pass
 
