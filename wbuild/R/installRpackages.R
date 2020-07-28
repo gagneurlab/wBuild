b50c85cca575464b5ef12301480db33febfc2362
Michaela Müller <mi.mueller@tum.de>
2020-07-29 00:14:26 +1000

    Installing R packages

---
+++ wbuild/R/installRpackages.R
@@ -0,0 +1,10 @@
+options(repos=structure(c(CRAN="https://cloud.r-project.org")))
+
+packages <- c("data.table", "DT", "gsubfn", "knitr", "magrittr", "methods", "stringr", "yaml")
+installed <- rownames(installed.packages())
+for (pckg_name in packages) {
+    if (pckg_name %in% installed)
+        message(paste(pckg_name, "already installed"))
+    else
+        install.packages(pckg_name)
+}
